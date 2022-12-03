48ff647c738a99b8e639aea7011907012571ba6a
yugegit <yukendran.sr03@gmail.com>
2022-12-03 22:22:45 +0530

    dashboard created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+
 import './App.css';
+import DashBoard from './Component/DashBoard';
+import SideBar from './Component/SideBar';
+import AllStudent from './Component/AllStudent';
+import AddStudent from './Component/AddStudent';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import EditStudent from './Component/EditStudent';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+
+  let data = {
+    earning: "40,000",
+    annual: "2,40,000",
+    task: "20",
+    pending: "26"
+
+  }
+
+  let [students,setStudents] = useState([])
+
+  
+
+  return <>
+  <Router>
+    <div style={{display: "grid",gridTemplateColumns:"20% 85%"}}>
+      <div>
+        <SideBar/>
+      </div>
+     <div>
+       <Routes>
+          <Route path='*' element={  <DashBoard data={data}/>} />
+
+          <Route path='/dashboard' element={  <DashBoard data={data}/>} />
+          <Route path='/all-student' element={<AllStudent data = {{students}}/>}/>
+          <Route path='/add-student' element={<AddStudent data = {{students,setStudents}}/>}/>
+          <Route path='/edit-student/:id' element={<EditStudent data = {{students,setStudents}}/>}/>
+          
+
+
+        </Routes>
+      </div>
     </div>
-  );
+ </Router>
+ 
+  </>
+  
 }
 
 export default App;
